a8bcf6c9967eb82c132bf89bd214181f9f502144
Vladyslav Danyliuk <vladyslav.danyliuk@student.tuke.sk>
2026-07-06 20:28:28 +0200

    crawler fix, sqlI fix

--- debug_crawler.py
+++
@@ -1,6 +0,0 @@
-from core.models import HttpClient
-from core.crawler import Crawler
-
-client = HttpClient(base_url="http://localhost:3000/#/")
-endpoints = Crawler(target_url=client.base_url, http_client=client).crawl()
-print("Found endpoints:", endpoints)
